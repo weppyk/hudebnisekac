281282aefb7047433184214a04475c29ac014cd6
Radek Drlik <radek.drlik@gmail.com>
2016-03-01 19:46:11 +0100

    added logout not working

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -10,38 +10,38 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class RegistrationController extends Controller
 {
-/**
-* @Route("/register", name="user_registration")
-*/
-public function registerAction(Request $request)
-{
-// 1) build the form
-$user = new User();
-$form = $this->createForm(UserType::class, $user);
+    /**
+    * @Route("/register", name="user_registration")
+    */
+    public function registerAction(Request $request)
+    {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
 
-// 2) handle the submit (will only happen on POST)
-$form->handleRequest($request);
-if ($form->isSubmitted() && $form->isValid()) {
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
 
-// 3) Encode the password (you could also do this via Doctrine listener)
-$password = $this->get('security.password_encoder')
-->encodePassword($user, $user->getPlainPassword());
-$user->setPassword($password);
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $this->get('security.password_encoder')
+            ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
 
-// 4) save the User!
-$em = $this->getDoctrine()->getManager();
-$em->persist($user);
-$em->flush();
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
 
-// ... do any other work - like send them an email, etc
-// maybe set a "flash" success message for the user
+            // ... do any other work - like send them an email, etc
+            // maybe set a "flash" success message for the user
 
-return $this->redirectToRoute('replace_with_some_route');
-}
+            return $this->redirectToRoute('homepage');
+        }
 
-return $this->render(
-'registration/register.html.twig',
-array('form' => $form->createView())
-);
+        return $this->render(
+        'registration/register.html.twig',
+        array('form' => $form->createView())
+    );
 }
 }
